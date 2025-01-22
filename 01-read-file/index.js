bb3fb02951799e53bd4f7103375de2bfdd4e9ec4
MashaPu <mariya_pudovkina@mail.ru>
2025-01-22 20:47:00 +0500

    fix: task HTML-bolder 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const output = require('node:process');
-const pathToTxt = __dirname + "\\text.txt";
+const pathToTxt = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(pathToTxt, 'utf-8');
 stream.on('data', (data) => {
   output.stdout.write(data);
